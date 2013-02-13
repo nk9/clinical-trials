8c3de89aa4de575013d193e26c532662e26aadc2
Nick Kocharhook <nick.kocharhook@skype.net>
2013-02-13 21:58:50 +0000

    Fleshed out the Python script. Process the date fields.

--- munge_xml.py
+++ munge_xml.py
@@ -1,22 +1,71 @@
 #!/usr/bin/python
 
 import os, os.path, subprocess
-
+from datetime import datetime
 def main():
-	firstLine = True
 	for root, _, files in os.walk("study_fields_xml"):
-		for f in files:
-			fullpath = os.path.join(root, f)
+		for index, file in enumerate(files):
+			fullpath = os.path.join(root, file)
 			
-			if firstLine:
-				print "\n".join(runXSLT(fullpath)[1:2])
-				firstLine = False
-			else:
-				print runXSLT(fullpath)[2]
+			doc = CTXMLDoc(fullpath)
+			if index == 0:
+				print doc.outputHeader()
 
-def runXSLT(path):
-	p = subprocess.check_output(["xsltproc", "process_xml.xslt", path])
-	return p.split("\n")
+			print doc.outputLine()
+		
+		print "\n" # end the file with a newline
+
+
+class CTXMLDoc(object):
+	def __init__(self, path):
+		self.path = path
+		self.fields = []
+		self.headerFields = ['NCT ID', 'Source', 'Recruitment', 'Interventions',
+							 'Start Date', 'Completion Date', 'Primary Completion Date',
+							 'Phase', 'Countries']
+		
+		# Run!
+		self.runXSLT()
+		self.processFields()
+	
+	def id(self):
+		return self.fields[0]
+	
+	def processFields(self):
+		# R is going to need 3-part dates, but the XML doesn't include a day. Sometimes.
+		# So we have to guess.
+		self.fields[4] = self.parseDate(self.fields[4])
+		self.fields[5] = self.parseDate(self.fields[5])
+		self.fields[6] = self.parseDate(self.fields[6])
+	
+	def parseDate(self, dateString):
+		outDateString = ""
+		
+		if len(dateString):
+			try:
+				outDate = datetime.strptime(dateString, '%B %d, %Y')
+			except Exception as e:
+				try:
+					outDate = datetime.strptime(dateString, '%B %Y')
+				except Exception as e:
+					print "Failed parsing Start Date for %s: '%s'" % (self.id(), dateString)
+			
+			outDateString = datetime.strftime(outDate, '%d/%m/%Y')
+		
+		return outDateString
+	
+	def runXSLT(self):
+		result = subprocess.check_output(["xsltproc", "process_xml.xslt", self.path])
+		fieldString = result.split("\n")[2] # 0 = XML declaration; 1 = header; 2 = the good stuff
+		
+		if (len(fieldString)):
+			self.fields = fieldString.split("\t")
+		
+	def outputHeader(self):
+		return "\t".join(self.headerFields)
+	
+	def outputLine(self):
+		return "\t".join(self.fields)
 
 # Default function is main()
 if __name__ == '__main__':
--- process_xml.xslt
+++ process_xml.xslt
@@ -2,8 +2,9 @@
 <xsl:stylesheet version="1.0" xmlns:fn="fn" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 
 	<xsl:template match="clinical_study">
-		<xsl:text>NCT ID&#x9;Recruitment&#x9;Interventions&#x9;Start Date&#x9;Completion Date&#x9;Primary Completion Date&#x9;Phase&#x9;Countries&#10;</xsl:text>
+		<xsl:text>NCT ID&#x9;Source&#x9;Recruitment&#x9;Interventions&#x9;Start Date&#x9;Completion Date&#x9;Primary Completion Date&#x9;Phase&#x9;Countries&#10;</xsl:text>
 		<xsl:value-of select="./id_info/nct_id" /><xsl:text>&#x9;</xsl:text>
+		<xsl:value-of select="./source" /><xsl:text>&#x9;</xsl:text>
 		<xsl:value-of select="./overall_status" /><xsl:text>&#x9;</xsl:text>
 		<xsl:value-of select="./intervention/intervention_type" separator=": " /><xsl:text>:&#x9;</xsl:text>
 		<xsl:value-of select="./start_date" /><xsl:text>&#x9;</xsl:text>
